f02234eac4ddd0aa174e752cc24b869b41012b91
Shivani Singh <91026976+ShivaniiSinghh@users.noreply.github.com>
2022-01-28 23:54:34 +0530

    Add files via upload

---
+++ RecursiveIndex.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class RecursiveIndex {
+    int n;
+    int [] arr;
+    RecursiveIndex(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of elements :");
+        n = s.nextInt();
+        arr = new int[n];
+        System.out.println("Enter the elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextInt();
+        }
+    }
+
+    public int indexFinder(int []a, int currentIndex, int searchvalue){
+        if(a[currentIndex] == searchvalue){
+            return currentIndex+1;
+        }
+        else
+            return indexFinder(a, currentIndex+1,searchvalue );
+    }
+
+}
+
+class Execution{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int cIndex = 0;
+        RecursiveIndex obj = new RecursiveIndex();
+        System.out.println("enter the element you want to search");
+        int sValue = s.nextInt();
+        int res = obj.indexFinder(obj.arr, cIndex,sValue);
+        System.out.println("the element is at the index of array :"+res);
+    }
+}
