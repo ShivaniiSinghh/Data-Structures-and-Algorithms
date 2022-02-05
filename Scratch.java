ce582dccde93f87b30a9506d6e24c7d90b41ac80
Shivani Singh <91026976+ShivaniiSinghh@users.noreply.github.com>
2022-02-05 23:15:36 +0530

    Add files via upload

---
+++ Scratch.java
@@ -0,0 +1,25 @@
+class Scratch {
+    // Remove character from the string
+    // input = "neeraj"
+    // output= "neerj"
+    // removing character a from the string
+
+    public static String remove(String str) {
+        // base case
+        if (str.length() == 0) {
+            return str;
+        }
+        String result = "";
+        // i think i mentioned == here insted of !=  in class
+        if (str.charAt(0) != 'a') {
+            result = result + str.charAt(0);
+        }
+        // recursive call
+        String recans = remove(str.substring(1));
+        return result + recans;
+    }
+    public static void main(String[] args) {
+        String name ="neeraj";
+        System.out.println(remove(name));
+    }
+}
